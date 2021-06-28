ed811416a72c112ce59a754fed80cdffee0f7a43
WangJi1127 <928210192@qq.com>
2021-06-28 22:40:07 +0800

    custom hook

--- react/jira/src/screens/project-list/index.jsx
+++ react/jira/src/screens/project-list/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import {List} from "./list";
 import {SearchPanel} from "./search-panel";
 import {useEffect, useState} from "react";
-import {cleanObject} from "../../utils";
+import {cleanObject, useDebounce, useMount} from "../../utils";
 import * as qs from "qs";
 
 const apiUrl = process.env.REACT_APP_API_URL;
@@ -14,22 +14,22 @@ export const ProjectListScreen = () => {
     })
     const [users, setUsers] = useState([])
     const [list, setList] = useState([])
-
+    const debounceParma = useDebounce(param, 2000);
     useEffect(() => {
-        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debounceParma))}`).then(async response => {
             if (response.ok) {
                 setList(await response.json())
             }
         })
-    }, [param])
+    }, [debounceParma])
 
-    useEffect(() => {
+    useMount(() => {
         fetch(`${apiUrl}/users`).then(async response => {
             if (response.ok) {
                 setUsers(await response.json())
             }
         })
-    }, [])
+    })
 
     return <div>
         <SearchPanel param={param} setParam={setParam} users={users}/>
--- react/jira/src/utils/index.js
+++ react/jira/src/utils/index.js
@@ -1,3 +1,5 @@
+import {useEffect, useState} from "react";
+
 export const isFalsy = (value) => value === 0 ? false : !value;
 
 export const cleanObject = (object) => {
@@ -10,4 +12,33 @@ export const cleanObject = (object) => {
         }
     })
     return result;
+}
+
+export const useMount = (callback) => {
+    useEffect(() => {
+        callback();
+    }, []) // [] 表示只执行一次
+}
+
+//防抖函数
+// const debounce = (func, delay) => {
+//     let timeout;
+//     return () => {
+//         if (timeout){
+//             clearTimeout();
+//         }
+//         timeout = setTimeout(function (){
+//             func();
+//         }, delay);
+//     }
+// }
+
+export const useDebounce = (value, delay) => {
+    const [debounceValue, setDebounceValue] = useState(value);
+    useEffect(() => {
+        const timeout = setTimeout(() => setDebounceValue(value), delay);
+        // 清除函数 在调用一个新的 effect 之前对前一个 effect 进行清理
+        return () => clearTimeout(timeout);
+    }, [value, delay])
+    return debounceValue;
 }
